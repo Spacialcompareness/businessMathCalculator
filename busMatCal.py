3e501a67f258ea2cb23a3b1561c1587ab5f60725
Spacialcompareness <broderickwhagglov@gmail.com>
2019-10-01 20:28:00 -0700

    Refactored two functions.

--- busMatCal.py
+++ busMatCal.py
@@ -9,59 +9,64 @@ options = {'Interest Total': '1.', 'Rate': '2.',
 
 #These are instructions.
 print('Simple Interest Calculator'.center(40, '-'))
-print('\nWhat can we help you find?\n'.center(40, ' '))
-#time.sleep(1.5) uncomment to put a time delay
-print('First, a few instructions.')
-#time.sleep(1.5) uncomment to put a time delay
+print('\nWhat can we help you find?'.center(40, ' '))
+print('\nFirst, a few instructions.')
+
 print('After each equation you will be brought back to the menu.\n' 
-        'From the main menu you can exit by pressing \'000\'. You have to get to the menu before you can exit.')
-#time.sleep(1.5) uncomment to put a time delay
-print('Do not enter any special characters, digits only. No dollar signs, commas, or % sigs.')
-#time.sleep(2.5) uncomment to put a time delay
-print('Press \'999\' from the menu to exit.')
+        'From the main menu you can exit by pressing \'000\'. You have to get to the menu before you can exit.\n'
+        'Do not enter any special characters, digits only. No dollar signs, commas, or % sigs.\n'
+        'Press \'999\' from the menu to exit.')
 
 #FUNCTIONS
 
+def getPrincipal():
+    principal = input(("Principal: Digits only. "))
+    return principal
+
+def getInterest():
+    interest = input("Interest: Digits only. ")
+    return interest
+
+def getRate():
+    rat = input("Interest rate per annum: Digits only. ")
+    return rat
+
+def getTime():
+    gettim = input("Is time in days(d), months(m) or years(y)? ")
+    if gettim == 'd':
+        tim = (int(input('How many days(d)? ')) / 365)
+        #gettim = int(input() / 365)
+        return tim
+    elif gettim == 'm':
+        tim = (int(input('How many months(m)? ')) / 12)
+        #gettim = int(input() / 12)
+        return tim
+    elif gettim == 'y':
+        tim = input('How many years(y)? ')
+        #gettim = input()
+        return tim
+    else: #TODO this does not work.
+        print('Not a valid option. Please select \'d\', \'m\', or \'y\'.')
+
 def simIntTot(): #This function finds the interest total in a simple interest calculation.
-    print("Principal: Digits only.")
-    principal = int(input())
-    print("Interest rate per annum? Digits only. ")
-    rate = (float(input()) / 100)
-    print('Is time in days(d) or months(m)? ')
-    time = input()
-    if time == 'd':
-        print('How many days? ')
-        time = (int(input()) / 365)
+    prin = int(getPrincipal())
+    rate = (float(getRate()) / int(100))
+    time = int(getTime())
 
-    else:
-        print('How many months? ')
-        time = (int(input()) / 12)
-
-    total = (principal * (1 + (rate * time)))
-    interest = ((principal * (1 + (rate * time))) - principal)
+    total = (prin * (1 + (rate * time)))
+    interest = ((prin * (1 + (rate * time))) - prin)
     print('The total is ${0:,.2f}.' .format(total))
     print('The interest is ${0:,.2f}.'.format(interest))
-    #time.sleep(2)
 
 def simIntRat(): #This function finds the rate in a simple interest calculation.
-    print("Principal: Digits only.")
-    principal = int(input())
-    print("Total interest: Digits only. ")
-    interest = float(input())
-    print('Is time in days(d) or months(m)? ')
-    time = input()
-    if time == 'd':
-        print('How many days? ')
-        time = (int(input()) / 365)
+    prin = int(getPrincipal())
+    inte = int(getInterest())
+    time = float(getTime())
 
-    else:
-        print('How many months? ')
-        time = (int(input()) / 12)
-
-    rate = ((100 * interest) / (principal * time))
-
+    rate = ((100 * inte) / (prin * time))
     print('The rate is {0:,.4f}%.' .format(rate))
 
+#TODO Refactor
 def simIntTim(): #This function finds the time in a simple interest calculation.
     print("Principal: Digits only.")
     principal = int(input())
@@ -88,6 +93,7 @@ def simIntTim(): #This function finds the time in a simple interest calculation.
     print('The time is {0:,.2f}'.format(time) + ' ' + str(timUni))
     #time.sleep(2)
 
+#TODO Refactor
 def simIntPrin(): # This function finds the principal in a simple interest calculation.
     print("Total interest: Digits only. ")
     interest = float(input())
@@ -114,7 +120,8 @@ def simIntPrin(): # This function finds the principal in a simple interest calcu
     #print('The interest is ${0:,.2f}.'.format(interest))
 
 while selection != 999:
-    print('\nYou are at the main menu. What would you like to find? ')
+    print('\nYou are at the main menu. What would you like to find?\n'
+          '\'999\' to exit.')
 
     def optionsFunc(optionAlign, leftWidth, rightWidth):
         for k, v in optionAlign.items():
